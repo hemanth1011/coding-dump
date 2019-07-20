3c8e625853085230eccc4b15e559d09b159c5cb4
Hemanth <hemanthmanikantabunga@gmail.com>
2019-07-20 14:56:47 +0530

    python : Update
    programs

---
+++ PowOfTwoNum.py
@@ -0,0 +1,7 @@
+num = int(input())
+pow = int(input())
+power = 1
+while(pow>=1):
+    power =power * num
+    pow -=1
+print(power)
---
+++ SqrKeyElement.py
@@ -0,0 +1,11 @@
+n = int(input("Enter no of elements:"))
+arr = []
+for i in range(n):
+     ele = int(input("Enter array element:"))
+     arr.append(ele)
+key = int(input("Enter Key element:"))
+for i in arr:
+    if(i>key**2):
+        print(i)
+    else:
+        print("No elements greater than Key")
