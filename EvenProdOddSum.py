3e31b4f40f4cabcc3efbf1237d395c78ecb1c54c
Hemanth <hemanthmanikantabunga@gmail.com>
2019-07-20 14:54:14 +0530

    python : Update
    - For loop & If Condition

--- EvenProdOddSum.py
+++ EvenProdOddSum.py
@@ -3,9 +3,10 @@ even = 1
 odd = 0
 l = len(num)
 for i in range(0,l,2):
-    even *= int(num[i])
-for j in range(1,l,2):
-    odd += int(num[j])
+    if(i%2==0):
+        even *= int(num[i])
+    else:
+        odd += int(num[i+1])
 if(even % odd == 0):
     print(True)
 else:
